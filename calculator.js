2a3ed1b5a85e2ebc43ba434190d07618b4337ca1
Leo <leolou.90@gmail.com>
2026-04-11 02:28:28 -0700

    finish functions for resolve multiplication and divisions, and addition and substraction

--- calculator.js
+++ calculator.js
@@ -19,12 +19,12 @@ const nextNumberAndResult = document.querySelector('.display_next_result');
 
 //Help functions
 
-function AddDecimal(digitToAddDecimal){
-    if(digitToAddDecimal === ""){
+function AddDecimal(digitToAddDecimal) {
+    if (digitToAddDecimal === "") {
         digitToAddDecimal += "0."
     }
 
-    if(!digitToAddDecimal.includes(".")){
+    if (!digitToAddDecimal.includes(".")) {
         digitToAddDecimal += ".";
     }
 
@@ -32,106 +32,157 @@ function AddDecimal(digitToAddDecimal){
 
 }
 
-function ResetCalculator(){
+function ResolveMultiplicationDivision(tokens) {
+
+    
+    for (let i = 0; i <= tokens.length; i++) {
+
+        if (tokens[i] === "*") {
+            let result = "";
+            let first = tokens[i - 1];
+            let second = tokens[i + 1];
+            result = Number(first) * Number(second);
+            tokens.splice(i - 1, 3, result.toString());
+            i -= 1;
+        }
+
+        if (tokens[i] === "/") {
+            let result = "";
+            let first = tokens[i - 1];
+            let second = tokens[i + 1];
+            result = Number(first) / Number(second);
+            tokens.splice(i - 1, 3, result.toString());
+            i -= 1;
+        }
+        
+    }
+    console.log(tokens);
+    return tokens;
+}
+
+function ResolveAdditionSubstraction(tokens) {
+    
+    for (let i = 0; i <= tokens.length; i++) {
+
+        if (tokens[i] === "+") {
+            let result = "";
+            let first = tokens[i - 1];
+            let second = tokens[i + 1];
+            result = Number(first) + Number(second);
+            tokens.splice(i - 1, 3, result.toString());
+            i -= 1;
+        }
+
+        if (tokens[i] === "-") {
+            let result = "";
+            let first = tokens[i - 1];
+            let second = tokens[i + 1];
+            result = Number(first) - Number(second);
+            tokens.splice(i - 1, 3, result.toString());
+            i -= 1;
+        }
+    }
+    return tokens;
+}
+
+
+function ResetCalculator() {
     Calculator.tokens = [];
     Calculator.current = "";
     Calculator.output = "";
     Render();
 }
 
-function DeleteCurrentNumber(){
+function DeleteCurrentNumber() {
     const currentNumber = Calculator.current.split("");
     currentNumber.pop();
     Calculator.current = currentNumber.join("");
     Render();
 }
 
-//Make a new array with all multiplications and divisions resolved
-function ResolveEquation(){
-    let currentResult = "";
-    const reducedTokens = [];
+function ResolveEquation() {
 
-    if(Calculator.current !== ""){
+    if (Calculator.current !== "") {
+
         Calculator.tokens.push(Calculator.current);
         Calculator.current = "";
     }
-
-    for(let i = 0; i < Calculator.tokens.length -1; i++){
-        if(Calculator.tokens[i] === "*" || Calculator.tokens[i] ==="/"){
-            const currentOperation = (Calculator.tokens[i-1] + Calculator.tokens[i] + Calculator.tokens[i+1]);
-            console.log(currentOperation);
-            i += 2;
-        }
-    }
+    let AdditionAndSubstractionTokens = ResolveMultiplicationDivision(Calculator.tokens);
+    
+    let EquationResolved = ResolveAdditionSubstraction(AdditionAndSubstractionTokens);
+    
+    Calculator.output = EquationResolved;
+    Render();
 }
 
 // Render functions
-function ThemeRender(){
+function ThemeRender() {
     Calculator.theme = `theme${slider.value}`
     body.classList = 'none';
     body.className = Calculator.theme;
 }
 
-function DisplayEquationRender(){
+function DisplayEquationRender() {
     equation.textContent = Calculator.tokens.join("");
 }
 
-function CurrentNumberRender(){
+function CurrentNumberRender() {
     nextNumberAndResult.textContent = Calculator.current;
 }
 
-function ResultRender(){
+function ResultRender() {
     nextNumberAndResult.textContent = Calculator.output;
 }
 
-function Render(){
+function Render() {
     ThemeRender();
     CurrentNumberRender();
     DisplayEquationRender();
 }
 
 
-slider.addEventListener('input', function(){
-   Render();
+//Event listeners
+
+slider.addEventListener('input', function () {
+    Render();
 });
 
-document.querySelectorAll(".digit").forEach(button =>{
-    button.addEventListener("click", () =>{
+document.querySelectorAll(".digit").forEach(button => {
+    button.addEventListener("click", () => {
         const value = button.dataset.value;
         Number(Calculator.current += value);
         Render();
     })
 });
 
-document.querySelector(".decimal").addEventListener("click", () =>{
+document.querySelector(".decimal").addEventListener("click", () => {
     const decimalNumber = AddDecimal(Calculator.current);
     Calculator.current = decimalNumber;
     Render();
 })
 
-document.querySelectorAll(".operator").forEach(button =>{
-    button.addEventListener("click", () =>{
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener("click", () => {
         const operator = button.dataset.value;
 
-        if(Calculator.current !== ""){
+        if (Calculator.current !== "") {
             Calculator.tokens.push(Calculator.current);
             Calculator.tokens.push(operator);
             Calculator.current = "";
             Render();
         }
-          
+
     })
 })
 
-document.querySelector("#reset_button").addEventListener("click", () =>{
+document.querySelector("#reset_button").addEventListener("click", () => {
     ResetCalculator();
 });
 
-document.querySelector("#delete_button").addEventListener("click", ()=>{
+document.querySelector("#delete_button").addEventListener("click", () => {
     DeleteCurrentNumber();
 });
 
-document.querySelector("#equal_button").addEventListener("click", () =>{
+document.querySelector("#equal_button").addEventListener("click", () => {
     ResolveEquation();
-    Render();
 });
